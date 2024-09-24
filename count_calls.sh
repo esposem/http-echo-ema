a5795711be1e87333782f2725d1a493d7ebce867
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2024-09-24 15:17:01 +0200

    add count_calls

---
+++ count_calls.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -e
+
+APP_URL=$(oc get routes/ema-http-echo-service -o jsonpath='{.spec.host}')
+
+declare -A string_set
+
+add_string() {
+    local str=$1
+    if [[ -v "string_set[$str]" ]]; then
+        ((string_set[$str]++))
+    else
+        string_set[$str]=1
+    fi
+}
+
+for pc in $(seq 1 100); do
+    x=$(curl -s $APP_URL | awk -F '-' '{print $NF}')
+    add_string $x
+    clear
+    for str in "${!string_set[@]}"; do
+        echo "# of pod $str replies: ${string_set[$str]}"
+    done
+done
